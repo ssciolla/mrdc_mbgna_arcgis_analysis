61862a37baebda029c47df892fd40efceda213a5
ssciolla <samgsciolla@gmail.com>
2019-04-29 23:17:05 -0400

    Refactoring; combining sql outputs; rename files; add README

---
+++ README.md
@@ -0,0 +1,30 @@
+# ArcGIS to MySQL Transformation Script
+
+#### Script Author(s): Sam Sciolla
+#### Created as part of the Migrating Research Data Collections project
+
+## Purpose
+
+This repository contains a Python script designed to convert the table and relationship JSON data about an ArcGIS database (fetched using a REST API) into a MySQL script with CREATE statements for the tables and layers. By using software such as [MySQL Workbench](https://www.mysql.com/products/workbench/), this script can be used to simply create an enhanced entity-relationship diagram.
+
+## Use
+
+To run this script, type the following at your command prompt of choice.
+
+`python transform_arcgis_json_to_sql.py`
+
+## Input
+
+This script has no file inputs. By default, the script will collect data from the URL for the Matthaei-Nichols Living Collections Database, a live ArcGIS database. To use the script on another database, simply change the values for `arcgis_database_base_url` and `new_database_name` under Main Program (at the end of the script). The URL should point to a Services Directory for the database. The [ArcGIS REST API](https://developers.arcgis.com/rest/services-reference/using-the-services-directory.htm) provides documentation on Services Directories and their use. `new_database_name` should contain the name of a MySQL database that the SQL output might be executed against.
+
+## Output
+
+This script will produce three files: `arcgis_response.json`, `relationship_matches.json`, and `database_tables.sql`. `arcgis_response.json` contains the JSON response received from the ArcGIS REST API (keep in mind responses are cached; delete the cache file to collect new data). `relationship_matches.json` stores reconfigured data about table relationships, resulting in records for each relationship that includes the names of both tables and cardinalities. `database_tables.sql` lays out the tables described in JSON as CREATE TABLE statments, ordered so that tables with foreign keys follow the tables they reference.
+
+## Dependencies
+
+This script uses the `[requests](https://2.python-requests.org/en/master/)` library to interact with the ArcGIS REST API. It also uses the `json` module, a standard library.
+
+## Rights
+
+...
--- transform_arcgis_json_to_sql.py
+++ transform_arcgis_json_to_sql.py
@@ -1,7 +1,7 @@
-## Script to transform ArcGIS JSON output into SQL statements for database comparison and analysis
 ## Migrating Research Data Collections Project
+## Script to transform ArcGIS database details in JSON into SQL statements
+## Script Author(s): Sam Sciolla
 ## Grant PI: Andrea Thomer
-## Script Author: Sam Sciolla
 
 ## Notes:
 # - If the database you are working with has many-to-many relationships, the script may need to be re-evaluated to ensure proper results.
@@ -11,7 +11,8 @@ import json
 import requests
 
 ## Global variables
-CACHE_FILE_NAME = 'mbgna_arcgis_cache.json'
+CACHE_FILE_NAME = 'arcgis_cache.json'
+PROTECTED_FIELD_NAMES = ['condition']
 
 # Setting up caching dictionary.
 try:
@@ -51,6 +52,7 @@ def fetch_API_data(base_url, parameters_diction):
 
 # Relationship analysis functions
 
+# Create a dictionary with information about relationships
 def parse_relationships(tables):
     relationship_matches = {}
     for table in tables:
@@ -59,13 +61,13 @@ def parse_relationships(tables):
             relat_id = relationship['id']
             if relat_id not in relationship_matches:
                 relationship_matches[relat_id] = {}
-            if 'Origin' in relationship['role'] and 'Origin' not in relationship_matches[relat_id]:
+            if 'Origin' in relationship['role']:
                 relationship_matches[relat_id]['Origin'] = relationship
                 origin_table_name = table['name'].replace(' ', '_')
                 if table['type'] in ['Feature Layer']:
                     origin_table_name += '_Layer'
                 relationship_matches[relat_id]['Origin Table Name'] = origin_table_name
-            elif 'Destination' in relationship['role'] and 'Destination' not in relationship_matches[relat_id]:
+            elif 'Destination' in relationship['role']:
                 relationship_matches[relat_id]['Destination'] = relationship
                 destination_table_name = table['name'].replace(' ', '_')
                 if table['type'] in ['Feature Layer']:
@@ -75,6 +77,7 @@ def parse_relationships(tables):
                 print("** Something's not working!! **")
     return relationship_matches
 
+# Determine the order of the tables in the SQL script based on their foreign key indexes
 def determine_table_order(relationship_matches_dict, table_name_strings):
     table_order_dict = {}
     for table_name in table_name_strings:
@@ -86,7 +89,7 @@ def determine_table_order(relationship_matches_dict, table_name_strings):
         origin_table_name = relationship_match['Origin Table Name']
         table_order_dict[dest_table_name]['Referenced Tables'].append(origin_table_name)
 
-    # Ordering tables
+    # Order tables
     next_index = 0
     tables_ordered = []
     tables_still_to_order = True
@@ -114,8 +117,7 @@ def determine_table_order(relationship_matches_dict, table_name_strings):
                             ref_table_num += 1
                         else:
                             break
-
-        # Assigning order indexes to tables in staging area
+        # Assign order indexes to tables in staging area
         staging_area = sorted(staging_area)
         for table_name in staging_area:
             table_order_dict[table_name]['Order Index'] = next_index
@@ -123,14 +125,11 @@ def determine_table_order(relationship_matches_dict, table_name_strings):
             tables_ordered.append(table_name)
         if len(tables_ordered) == len(table_order_dict.keys()):
             tables_still_to_order = False
+
     return table_order_dict
 
-def write_create_table_statement(table_dict, relationship_matches):
-    # Name table
-    table_name = table_dict['name'].replace(' ', '_')
-    if 'Layer' in table_dict['type']:
-        table_name += '_Layer'
-
+# Generate a SQL CREATE statement as a single multi-line string
+def write_create_table_statement(table_dict, new_table_name, relationship_matches):
     # Determine foreign key lines and store index info for later
     local_origin_keys = []
     unique_destination_keys = []
@@ -157,7 +156,9 @@ def write_create_table_statement(table_dict, relationship_matches):
         extras = None
         data_type = None
         field_name = field['name']
-
+        # If field name is known to be protected, add an underscore to the end of the field name
+        if field_name.lower() in PROTECTED_FIELD_NAMES:
+            field_name += '_'
         if 'String' in field['type']:
             length = field['length']
             data_type = 'VARCHAR({})'.format(str(length))
@@ -168,7 +169,7 @@ def write_create_table_statement(table_dict, relationship_matches):
         elif 'Date' in field['type']:
             data_type = 'DATE'
         elif 'GUID' in field['type']:
-            print("** GUID data type found! {} {} **".format(table_name, field['name']))
+            print("** GUID data type found! {} {} **".format(new_table_name, field['name']))
             data_type = 'INTEGER'
         elif 'OID' in field['type']:
             data_type = 'INTEGER'
@@ -194,7 +195,7 @@ def write_create_table_statement(table_dict, relationship_matches):
             attribute_line += ' ' + extras
         attribute_lines.append(attribute_line)
 
-    # Primary key line
+    # Write primary key line
     primary_key_line = '    PRIMARY KEY ({})'.format(id_attribute_name)
 
     # Put everything together
@@ -207,83 +208,63 @@ CREATE TABLE IF NOT EXISTS {}
   )
 ENGINE=InnoDB
 CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;'''.format(table_name, main_block)
+COLLATE utf8mb4_0900_ai_ci;'''.format(new_table_name, main_block)
     return create_statement
 
-def create_preamble(table_list):
+# Create string with SQL preamble containing a list of tables to drop and the name of the database to use
+def create_preamble(table_list, database_name):
     tables_string = ", ".join(table_list)
-    preamble = '''
-USE MBGNA_ArcGIS;
-
+    preamble = '''USE {};
 SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS {};
 SET FOREIGN_KEY_CHECKS=1;
-               '''.format(tables_string)
+'''.format(database_name, tables_string)
     return preamble
+
+# Run the transformation workflow on a ArcGIS database specified by URL
+def run_transformation(database_base_url, database_name):
+    # Pulling data from ArcGIS Server
+    parameters = {'f':'json'}
+    data = fetch_API_data(database_base_url, parameters)
+    layers = data['layers']
+    tables = data['tables']
+    all_tables = layers + tables
+    arcgis_json_file = open('arcgis_response.json', 'w', encoding='utf-8')
+    arcgis_json_file.write(json.dumps(data, indent=4))
+    arcgis_json_file.close()
+
+    # Identify pairs of tables connected through relationships
+    relationship_matches = parse_relationships(all_tables)
+    relationship_matches_file = open('relationship_matches.json', 'w', encoding='utf-8')
+    relationship_matches_file.write(json.dumps(relationship_matches, indent=4))
+    relationship_matches_file.close()
+
+    # Produce SQL script statments
+    sql_statements = {}
+    for table in all_tables:
+        table_name = table['name'].replace(' ', '_')
+        if 'Layer' in table['type']:
+            table_name += '_Layer'
+        print('// {} //'.format(table_name))
+        table_statement = write_create_table_statement(table, table_name, relationship_matches)
+        sql_statements[table_name] = table_statement
+    all_table_names = sql_statements.keys()
+
+    # Write SQL script
+    ordering_result = determine_table_order(relationship_matches, all_table_names)
+
+    all_tables_in_order = sorted(sql_statements.keys(), key=lambda x: ordering_result[x]['Order Index'])
+    sql_text = create_preamble(all_table_names, database_name)
+    for next_table_name in all_tables_in_order:
+        sql_text += '\n' + sql_statements[next_table_name] + '\n'
+    tables_sql_file = open('database_tables.sql', 'w')
+    tables_sql_file.write(sql_text)
+    tables_sql_file.close()
 
 ## Main Program
 
-print('-- ArcGIS JSON to MySQL Transformation Script --')
-
-# Pulling data from ArcGIS Server
-
-base_url = 'https://services1.arcgis.com/4ezfu5dIwH83BUNL/arcgis/rest/services/MBGNA_Database_Local/FeatureServer//layers'
-parameters = {'f':'json'}
-data = fetch_API_data(base_url, parameters)
-
-arcgis_json_file = open('arcgis_json_file.json', 'w', encoding='utf-8')
-arcgis_json_file.write(json.dumps(data, indent=4))
-arcgis_json_file.close()
-
-layers = data['layers']
-tables = data['tables']
-all_tables = layers + tables
-
-# Identifying pairs of tables connected through relationships
-
-relationship_matches = parse_relationships(all_tables)
-
-relationship_matches_file = open('relationship_matches.json', 'w', encoding='utf-8')
-relationship_matches_file.write(json.dumps(relationship_matches, indent=4))
-relationship_matches_file.close()
-
-# Producing SQL script statments
-
-table_sql_statements = {}
-table_names = []
-for table in tables:
-    table_name = table['name'].replace(' ', '_')
-    print('// {} //'.format(table_name))
-    table_names.append(table_name)
-    table_statement = write_create_table_statement(table, relationship_matches)
-    table_sql_statements[table_name] = table_statement
-
-layer_sql_statements = {}
-layer_names = []
-for layer in layers:
-    layer_name = layer['name'].replace(' ', '_') + '_Layer'
-    print('// {} //'.format(layer_name))
-    layer_names.append(layer_name)
-    layer_statement = write_create_table_statement(layer, relationship_matches)
-    layer_sql_statements[layer_name] = layer_statement
-
-# Writing SQL scripts
-
-all_table_names = table_names + layer_names
-ordering_result = determine_table_order(relationship_matches, all_table_names)
-
-tables_order = sorted(table_sql_statements.keys(), key=lambda x: ordering_result[x]['Order Index'])
-tables_sql_text = create_preamble(table_names)
-for table_name in tables_order:
-    tables_sql_text += '\n' + table_sql_statements[table_name]
-tables_sql_file = open('mbgna_arcgis_one.sql', 'w')
-tables_sql_file.write(tables_sql_text)
-tables_sql_file.close()
-
-layers_order = sorted(layer_sql_statements.keys(), key=lambda x: ordering_result[x]['Order Index'])
-layers_sql_text = create_preamble(layer_names)
-for layer_name in layers_order:
-    layers_sql_text += '\n' + layer_sql_statements[layer_name]
-layers_sql_file = open('mbgna_arcgis_two.sql', 'w')
-layers_sql_file.write(layers_sql_text)
-layers_sql_file.close()
+if __name__ == '__main__':
+    print('** ArcGIS JSON to MySQL Transformation Script **')
+    arcgis_database_base_url = 'https://services1.arcgis.com/4ezfu5dIwH83BUNL/arcgis/rest/services/MBGNA_Database_Local/FeatureServer//layers'
+    new_database_name = 'MBGNA_ArcGIS'
+    run_transformation(arcgis_database_base_url, new_database_name)
